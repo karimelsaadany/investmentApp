d759b496e1773065336d61a1f703ade716b82755
Karim Elsaadany <kasaadany@gmail.com>
2021-11-21 14:44:36 +0200

    updated dist folder

--- dist/_nuxt/static/1637495175/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637495175",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},state:{layout:"Cockpit"},serverRendered:false,routePath:a,config:{_app:{basePath:a,assetsPath:"\u002F_nuxt\u002F",cdnURL:null}}}}("\u002F"));
